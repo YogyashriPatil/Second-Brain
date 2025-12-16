b7badb2bf838e17912c83ca11e5de6f64cd65533
YogyashriPatil <yogyashripatil20@gmail.com>
2025-12-16 09:36:05 +0530

    Create the dashborad

---
+++ Frontend/src/pages/Dashboard.tsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import { Button } from '../components/ui/Button'
+import { PlusIcon } from '../icons/PlusIcon'
+import { ShareIcon } from '../icons/ShareIcon'
+import { Card } from '../components/ui/Card'
+import { Doc } from '../icons/Doc'
+import { CreateContentModel } from '../components/CreateContentModel'
+import { SideBar } from '../components/Sidebar'
+
+export function Dashboard() {
+  const [modalOpen, setModalOpen] = useState(true);
+
+  return (
+    <div className='bg-purple-50'>
+      <SideBar />
+      <div className='p-4 ml-72 min-h-screen bg-gray-100 border-2'>
+        <CreateContentModel open={modalOpen} onClose={() => {
+          setModalOpen(false)
+        }} />
+        <div className='flex justify-end gap-4'>
+            <Button startIcon={<ShareIcon size='lg'/>}  size='sm' variant='primary' text='Share Brain' />
+            <Button onClick={() => {
+              setModalOpen(true)
+            }} startIcon={<PlusIcon size='lg' />} size='sm' variant='secondary' text='Add Content' />
+        </div>
+        Second Brain
+        <div className='flex gap-3'>
+          <Card title='First tweet' link='https://x.com/YogyashriP/status/1963830234722992432' type='twitter'/>
+          <Card title='video that i want to watch' link='https://www.youtube.com/embed/N5LkiQ4_nT0?si=6Lui3doR3PBa1yD8' type='youtube' />
+        </div>
+      </div>
+    </div>
+  )
+}
+
